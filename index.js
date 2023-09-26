c6f51be782f35e6c1c18a3a354a13ac3c73f62db
adiMallya <55098608+adiMallya@users.noreply.github.com>
2023-09-26 09:05:18 +0000

    refactor: API security protections

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xssSanitize = require('xss-clean');
+const rateLimit = require('express-rate-limit');
 const connectDatabase = require('./config/db');
 const errorHandler = require('./middlewares/error.middleware');
 
@@ -20,8 +23,13 @@ app.use(express.json());
 if (process.env['NODE_ENV'] === 'development') {
   app.use(logger);
 }
-
+// Sanitize data(prevent NoSQL injections)
+app.use(mongoSanitize());
+// Prevent XSS attacks
+app.use(xssSanitize());
+// Enable CORS(Cross-Origin Resource Sharing)
 app.use(cors({}));
+
 app.use(helmet())
 
 //Mount routes
